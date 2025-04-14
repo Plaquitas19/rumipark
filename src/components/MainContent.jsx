06ac22f0f1d39def2704dd02f27e07fb7ac89de5
Plaquitas19 <plaquitasvehiculares@gmail.com>
2025-04-14 04:17:41 -0500

    Update MainContent.jsx

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { io } from "socket.io-client";
 import { useNavigate } from "react-router-dom";
 import VehicleTable from "./VehicleTable";
 import CameraSection from "./CameraSection";
 
 function MainContent() {
-  const [estadoVehiculos, setEstadoVehiculos] = useState([]);
+  const [estadoVehiculos] = useState([]);
   const [currentTime, setCurrentTime] = useState("");
-  const [saludo, setSaludo] = useState("");
-  const [username, setUsername] = useState("");
-
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,85 +13,34 @@ function MainContent() {
     if (!localStorage.getItem("auth_token")) {
       navigate("/"); // Redirige al login si no hay token
     }
-
-    // Obtener el username desde localStorage
-    const usuarioUsername = localStorage.getItem("username");
-    setUsername(usuarioUsername);
-
-    // Obtener la hora actual en la zona horaria de Perú para el saludo
-    const ahora = new Date();
-    const optionsSaludo = {
-      timeZone: "America/Lima",
-      hour: "2-digit",
-      minute: "2-digit",
-      hour12: false, // Asegurarse de usar el formato de 24 horas
-    };
-
-    // Extraer la hora en formato 24 horas desde la zona horaria de Perú
-    const horaPeru = new Intl.DateTimeFormat("es-PE", optionsSaludo).format(
-      ahora
-    );
-    const hora = parseInt(horaPeru.split(":")[0], 10);
-
-    // Determinar el saludo según la hora
-    if (hora >= 0 && hora < 12) {
-      setSaludo("Buenos días");
-    } else if (hora >= 12 && hora < 18) {
-      setSaludo("Buenas tardes");
-    } else {
-      setSaludo("Buenas noches");
-    }
   }, [navigate]);
 
   useEffect(() => {
-    // Actualizar la hora y fecha cada segundo, sin tocar la zona horaria para el saludo
+    // Actualizar la hora y fecha cada segundo
     const interval = setInterval(() => {
       const now = new Date();
-      const formattedTime = now.toLocaleString(); // Esto utiliza la hora local actual, sin modificaciones de zona
+      const formattedTime = now.toLocaleString(); // Usa la hora local
       setCurrentTime(formattedTime);
     }, 1000);
 
     return () => clearInterval(interval);
   }, []);
 
-  useEffect(() => {
-    const socket = io("http://localhost:5000");
-
-    socket.on("actualizar_estado", (data) => {
-      setEstadoVehiculos((prevState) => [...prevState, data]);
-    });
-
-    return () => {
-      socket.off("actualizar_estado");
-      socket.close();
-    };
-  }, []);
-
   return (
     <div className="w-full h-full min-h-screen p-5 bg-gray-100 overflow-auto">
-      <header className="text-lg font-bold mb-4 flex items-center justify-between px-4">
-        <div className="flex items-center gap-2">
-          <span className="text-[#167f9f]">{saludo}</span>,{" "}
-          <span className="text-[#167f9f]">
-            {username ? username : "Usuario"}
-          </span>
-        </div>
-      </header>
-
       <div
         className="bg-gray-200 rounded-lg p-4 mb-6 shadow-md border-4 w-full"
         style={{ borderColor: "#167f9f" }}
       >
-        <div className="flex justify-between items-center mb-4">
-          <h2 className="text-xl font-bold text-gray-700">
+        <div className="flex flex-col items-center mb-4 md:flex-row md:justify-between">
+          <h2 className="text-xl font-bold text-gray-700 text-center">
             Detección de Placas
           </h2>
-          <div className="flex items-center text-lg text-gray-600">
+          <div className="flex items-center text-lg text-gray-600 mt-2 md:mt-0">
             <i className="fas fa-calendar-alt mr-2 text-blue-600"></i>
-            <span className="mr-4">{currentTime.split(",")[0]}</span>{" "}
-            {/* Mostrar la fecha */}
+            <span className="mr-4">{currentTime.split(",")[0]}</span>
             <i className="fas fa-clock mr-2 text-green-600"></i>
-            <span>{currentTime.split(",")[1]}</span> {/* Mostrar la hora */}
+            <span>{currentTime.split(",")[1]}</span>
           </div>
         </div>
         <CameraSection />
